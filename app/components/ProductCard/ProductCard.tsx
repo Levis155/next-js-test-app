e6558b80ed3794ebfb4fa2570ecaf09cfebeae16
Levis155 <levismbui@gmail.com>
2025-09-01 08:29:40 +0300

    first commit

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,12 @@
+import AddToCart from "../AddToCart";
+import styles from "./ProductCard.module.css"
+
+const ProductCard = () => {
+    return(
+        <div className={styles.cardContainer}>
+            <AddToCart />
+        </div>
+    )
+}
+
+export default ProductCard;
