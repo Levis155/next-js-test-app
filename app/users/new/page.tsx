fd0136aaceaa7823fc2411ae339fe973b35e54be
Levis155 <levismbui@gmail.com>
2025-09-01 12:58:59 +0300

    add exercises on programmatic navigation

---
+++ app/users/new/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { use } from 'react'
+
+const NewUserPage = () => {
+    const router = useRouter();
+
+  return (
+    <button className='btn btn-primary' onClick={() => router.push('/users')}>Create</button>
+  )
+}
+
+export default NewUserPage
