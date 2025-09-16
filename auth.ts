2e7399344330349744e1ad4adfc52e1cda143621
Levis155 <levismbui@gmail.com>
2025-09-16 08:27:48 +0300

    add exercise on configuring credentialsProvider

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./prisma/client";
+import bcrypt from 'bcrypt';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -9,6 +11,27 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "Email" },
+        password: { label: "Password", type: "password", placeholder: "Password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string}
+        })
+
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(credentials.password as string, user.hashedPassword as string);
+
+        return passwordsMatch ? user : null;
+      },
     }),
   ],
 });
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { auth } from "@/auth"
 
 export default auth((req) => {
   // If the user is not authenticated, redirect them to the login page.
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
+  if (!req.auth) {
+    const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 
@@ -12,5 +12,5 @@ export default auth((req) => {
 
 // Configure which paths to protect.
 export const config = {
-  matcher: ["/users/:id*"], // Protect all routes under `/users`.
+  matcher: ["/users/:id*"], // Protect all routes under `users`.
 };
