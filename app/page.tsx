b5b53d83b92c687ec2895519d78026c8b592d469
Levis155 <levismbui@gmail.com>
2025-09-17 07:37:22 +0300

    add exercise on optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
-import { auth } from "@/auth";
+import Image from "next/image";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="react cover"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
-  )
+  );
 }
