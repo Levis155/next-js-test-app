6c2ba3c345646a9077eb9c74e1f077e6f383b401
Levis155 <levismbui@gmail.com>
2025-09-17 09:19:57 +0300

    add exercise on using fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Script from "next/script";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google"; //Inter font imported
+import localFont from "next/font/local";
 import Navbar from "./Navbar";
 import AuthProvider from "./Provider";
 import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500']
+})
+
+const poppins = localFont({
+  src: '../public/fonts/poppins-regular-webfont.woff2',
+  variable: '--font-poppins'
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AuthProvider>
           <Navbar />
           <main className="p-5">{children}</main>
